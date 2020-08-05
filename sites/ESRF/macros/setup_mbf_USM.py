74621424c365e17e5c1ed811d36d6732aace8655
abdomit <benoit.roche@esrf.fr>
2020-08-05 21:38:30 +0200

    Don't change FE gain automatically

--- sites/ESRF/macros/setup_mbf_USM.py
+++ sites/ESRF/macros/setup_mbf_USM.py
@@ -339,8 +339,9 @@ class MBF_HL():
             return "ON"
 
     def comm_set_feedback_on(self, state=True):
-        reload(external_devices)
-        external_devices.set_highgain(state)
+        # protect equipment against single bunches
+        #reload(external_devices)
+        #external_devices.set_highgain(state)
 
         Mbf = self.Mbf
         if state == True:
