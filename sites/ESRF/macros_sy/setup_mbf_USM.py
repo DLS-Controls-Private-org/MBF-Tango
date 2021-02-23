ca7f2b1319fec13ba30e2103b440b9b6cb16e0de
Benoît Roche <benoit.roche@esrf.fr>
2021-02-23 12:21:27 +0100

    Macros for SY cleaning (for JLP last appli)

--- sites/ESRF/macros_sy/setup_mbf_USM.py
+++ sites/ESRF/macros_sy/setup_mbf_USM.py
@@ -1,7 +1,9 @@
 from tango import *
 from numpy import *
+import numpy as np
 import time
 from importlib import reload
+from math import modf
 
 
 class MBF_HL():
@@ -16,7 +18,7 @@ class MBF_HL():
         else:
             Mbf.put('SEQ:1:ENABLE_S', 'Off')
 
-    def set_sweep(self):
+    def set_sweep(self, harmonic_shift=0):
         Mbf = self.Mbf
         mbfCtrl = self.mbfCtrl
 
@@ -32,16 +34,16 @@ class MBF_HL():
         if Harmonic < 0:
             Harmonic = abs(Harmonic)
             tune_sweep += 0.5
+        Harmonic += harmonic_shift
         sweep_range = mbfCtrl.SweepRange
         sweep_start = Harmonic + tune_sweep - sweep_range
         sweep_end = sweep_start + 2 * sweep_range
-        Mbf.put('SEQ:1:COUNT_S', 50)
         Mbf.put('SEQ:1:START_FREQ_S', sweep_start)
         Mbf.put('SEQ:1:END_FREQ_S', sweep_end)
         Mbf.put('SEQ:1:CAPTURE_S', 'Capture')
         Mbf.put('SEQ:1:HOLDOFF_S', 0)
-        Mbf.put('SEQ:1:DWELL_S', mbfCtrl.SweepDwellTime)
-        Mbf.put('SEQ:1:GAIN_SCALAR_S', mbfCtrl.FeedbackFineGain)
+        #Mbf.put('SEQ:1:DWELL_S', mbfCtrl.SweepDwellTime)
+        #Mbf.put('SEQ:1:GAIN_SCALAR_S', mbfCtrl.FeedbackFineGain)
         Mbf.put('SEQ:1:ENWIN_S', 'Windowed')
         Mbf.put('SEQ:1:BLANK_S', 'Off')
 
@@ -57,14 +59,86 @@ class MBF_HL():
         modeList = mbfCtrl.ModeList
         mode = modeList[mbfCtrl.mode]
         mbfDevName = Mbf.mbfDevName
+        BUNCH_COUNT = Mbf.bunch_count
 
-        self.set_sweep()
-
+        gainwf_sweep = np.zeros((704,))
         if mode == '7/8_1b':
-            Mbf.put('SEQ:1:BANK_S', 0)
+            bank_this_mode = 0
+            gainwf_sweep[1:352] = 1
+            gainwf_sweep[352+1:352+352] = -1
+            Mbf.put('SEQ:1:BANK_S', bank_this_mode)
+            harmonic_shift = 1
         elif mode == '7/8_2b':
-            Mbf.put('SEQ:1:BANK_S', 1)
+            bank_this_mode = 1
+            gainwf_sweep[1:206] = 1
+            gainwf_sweep[207:352] = -1
+            gainwf_sweep[352+1:352+206] = 1
+            gainwf_sweep[352+207:352+352] = -1
+            Mbf.put('SEQ:1:BANK_S', bank_this_mode)
+            harmonic_shift = 0
         elif mode == '7/8_4b':
-            Mbf.put('SEQ:1:BANK_S', 2)
+            bank_this_mode = 2
+            gainwf_sweep[1:88] = 1
+            gainwf_sweep[89:176] = -1
+            gainwf_sweep[177:264] = 1
+            gainwf_sweep[265:352] = -1
+            gainwf_sweep[352+1:352+88] = 1
+            gainwf_sweep[352+89:352+176] = -1
+            gainwf_sweep[352+177:352+264] = 1
+            gainwf_sweep[352+265:352+352] = -1
+            Mbf.put('SEQ:1:BANK_S', bank_this_mode)
+            harmonic_shift = 0
+        elif mode == '16-bunch':
+            bank_this_mode = 0
+            gainwf_sweep[1:62] = 1
+            gainwf_sweep[63:124] = -1
+            gainwf_sweep[125:186] = 1
+            gainwf_sweep[187:248] = -1
+            gainwf_sweep[249:352] = 1
+            gainwf_sweep[352+1:352+62] = -1
+            gainwf_sweep[352+63:352+124] = 1
+            gainwf_sweep[352+125:352+186] = -1
+            gainwf_sweep[352+187:352+248] = 1
+            gainwf_sweep[352+249:352+352] = -1
+            Mbf.put('SEQ:1:BANK_S', bank_this_mode)
+            harmonic_shift = 1
+        elif mode == '4-bunch':
+            bank_this_mode = 0
+            gainwf_sweep[1:248] = 1
+            gainwf_sweep[249:352] = -1
+            gainwf_sweep[352+1:352+248] = 1
+            gainwf_sweep[352+249:352+352] = -1
+            Mbf.put('SEQ:1:BANK_S', bank_this_mode)
+            harmonic_shift = 0
+        elif mode == 'ARB_Pattern':
+            raise Exception(('ARB_Pattern mode is not implemented'
+                    ).format(BUNCH_COUNT))
+            bank_this_mode = 0
+            user_pattern = mbfCtrl.CleaningPattern
+            if user_pattern.size != BUNCH_COUNT:
+                raise ValueError(('CleaningPattern should have exactly {:.0f} '
+                        + 'elements').format(BUNCH_COUNT))
+            Mbf.put('SEQ:1:BANK_S', bank_this_mode)
+            harmonic_shift = 0
+
+        BUNCH_ZEROS = np.zeros(BUNCH_COUNT, dtype=int)
+        BUNCH_ONES = np.ones(BUNCH_COUNT, dtype=int)
+        all_bucket = np.ones((BUNCH_COUNT,))
+        for bank in range(4):
+            prefix = 'BUN:{:d}'.format(bank)
+            Mbf.put(prefix + ':FIRWF_S', BUNCH_ZEROS)
+            Mbf.put(prefix + ':FIR:ENABLE_S', BUNCH_ZEROS)
+            Mbf.put(prefix + ':NCO1:ENABLE_S', BUNCH_ZEROS)
+            Mbf.put(prefix + ':NCO2:ENABLE_S', BUNCH_ZEROS)
+            Mbf.put(prefix + ':SEQ:ENABLE_S', BUNCH_ONES)
+            Mbf.put(prefix + ':PLL:ENABLE_S', BUNCH_ZEROS)
+            Mbf.put(prefix + ':FIR_GAIN_S', 0*all_bucket)
+            Mbf.put(prefix + ':NCO1:GAIN_S', 0*all_bucket)
+            Mbf.put(prefix + ':NCO2:GAIN_S', 0*all_bucket)
+            Mbf.put(prefix + ':PLL:GAIN_S', 0*all_bucket)
+        prefix = 'BUN:{:d}'.format(bank_this_mode)
+        Mbf.put(prefix + ':SEQ:GAIN_S', gainwf_sweep)
+
+        self.set_sweep(harmonic_shift)
 
         return str_warning
