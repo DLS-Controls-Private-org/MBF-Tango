2faa943afc837ac826a2d1658bbf1fc07a2398b1
Claudio Scafuri <claudio.scafuri@elettra.eu>
2021-04-26 15:38:05 +0200

    adapted to Elettra naming convention

---
+++ sites/Elettra/tango/config.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+
+horizontal_axis_number = 1
+
+tmbf_dev_name = {
+        'horizontal': 'tmbf/processor/h',
+        'vertical': 'tmbf/processor/v',
+        'global': 'tmbf/processor/global' }
+
+tmbf_instance_name = {
+        'horizontal': 'tmbf-h',
+        'vertical': 'tmbf-v',
+        'global': 'tmbf-global' }
+
+tfit_dev_name = {
+        'horizontal': 'tmbf/tune_fit/h',
+        'vertical': 'tmbf/tune_fit/v' }
+
+tfit_instance_name = {
+        'horizontal': 'tmbf-tuneh',
+        'vertical': 'tmbf-tunev' }
+
+vars_users = {
+        'DEVICE': 'SR-TMBF',
+        'AXIS0': 'Y',
+        'AXIS1': 'X',
+        'MEMORY_READOUT_LENGTH': '16384',
+        'ADC_TAPS': '20',
+        'DAC_TAPS': '20',
+        'BUNCHES_PER_TURN': '432',
+        'DETECTOR_LENGTH': '4096',
+        'BUNCH_TAPS': '16',
+        'TUNE_PLL_LENGTH': '4096',
+        'REVOLUTION_FREQUENCY': '1156606' }
+
+
+# vim: set filetype=python:
