b83ec684b7b254c2b13907ff103b5633e68586c4
Benoît ROCHE <benoit.roche@esrf.fr>
2022-11-10 15:06:11 +0100

    Fix mistake for modes '28*12+1' and '24*8+1'

--- sites/ESRF/macros/setup_mbf_USM.py
+++ sites/ESRF/macros/setup_mbf_USM.py
@@ -278,7 +278,7 @@ class MBF_HL():
             clean_pattern[1:1+gap_l] = 1
             clean_pattern[-gap_r:] = -1
             start = gap_l+trains_l+1
-            for ii in range(28):
+            for ii in range(27):
                 clean_pattern[start+ii*31:start+ii*31+(31-trains_l)] = \
                         (2*(ii%2)-1)
             # Feedback on single bunchv$ (FIR #0)
@@ -287,13 +287,13 @@ class MBF_HL():
             fb_patterns[1][:] = clean_pattern == 0
             fb_patterns[1][0] = 0
         elif sr_mode == '24*8+1':
-            gap_l = 148
+            gap_l = 147
             gap_r = 123
             trains_l = 8
             clean_pattern[1:1+gap_l] = 1
             clean_pattern[-gap_r:] = -1
             start = gap_l+trains_l+1
-            for ii in range(24):
+            for ii in range(23):
                 clean_pattern[start+ii*31:start+ii*31+(31-trains_l)] = \
                         (2*(ii%2)-1)
             # Feedback on single bunchv$ (FIR #0)
